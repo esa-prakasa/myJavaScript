89bb808035e1d287b5e5dbcb38915d0c8f95a526
Esa Prakasa <esa.prakasa@gmail.com>
2020-04-25 22:42:43 +0700

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,27 @@
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+function my2ndFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph berubah Oii.";
+}
+
+function satu() {
+  document.getElementById("angkaku").innerHTML = "Satu ekor sapi";
+}
+
+function dua() {
+  document.getElementById("angkaku").innerHTML = "Dua sisi mata uang";
+}
+
+function tiga() {
+  document.getElementById("angkaku").innerHTML = "Tiga butir permen";
+}
+
+function empat() {
+  document.getElementById("angkaku").innerHTML = "Empat roda mobil";
+}
+
+function lima() {
+  document.getElementById("angkaku").innerHTML = "Lima jari tangan";
+}
